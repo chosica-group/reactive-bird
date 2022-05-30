23960bc97d8646cb4eaa52559390daa1582312b2
Andrey.A <79037128326@ya.ru>
2022-05-30 17:58:29 +0300

    feat: configuration webpack

---
+++ webpack/common.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const { distPath, rootPath, srcPath } = require('./path');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  context: process.cwd(),
+  entry: {
+    main: path.resolve(srcPath, 'index.tsx'),
+  },
+  output: {
+    path: distPath,
+    filename: '[name].js',
+    publicPath: '/',
+    assetModuleFilename: 'static/images/[name][ext]',
+  },
+  resolve: {
+    // TODO добавить алиасы при необходимости
+    alias: {},
+    extensions: ['.tsx', '.ts', '.js'],
+    symlinks: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: '@svgr/webpack',
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new Dotenv({
+      path: path.resolve(rootPath, '.env'),
+      allowEmptyValues: false,
+    }),
+  ],
+};
