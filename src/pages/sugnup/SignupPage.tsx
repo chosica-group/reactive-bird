d5a9c0aebed0dd06f8384f88f1be6675a62e8088
Elena Kuznetsova <elenakuznetsova177@yandex.ru>
2022-06-07 22:36:44 +0300

    variable inputs put out of SignUpPage

--- src/pages/sugnup/SignupPage.tsx
+++ src/pages/sugnup/SignupPage.tsx
@@ -1,19 +1,18 @@
 import { CardComponent } from './components/card/index';
 import './SignUpPage.css';
 
-export const SignUpPage = () => {
-  const inputs = [
-    { name: 'email', label: 'Почта', type: 'email' },
-    { name: 'login', label: 'Логин', type: 'text' },
-    { name: 'first_name', label: 'Имя', type: 'text' },
-    { name: 'second_name', label: 'Фамилия', type: 'text' },
-    { name: 'phone', label: 'Телефон', type: 'text' },
-    { name: 'password', label: 'Пароль', type: 'password' },
-    { name: 'passwordRepeat', label: 'Пароль (еще раз)', type: 'password' },
-  ];
-  return (
-    <div className="signup-page">
-      <CardComponent inputs={inputs} />
-    </div>
-  );
-};
+const inputs = [
+  { name: 'email', label: 'Почта', type: 'email' },
+  { name: 'login', label: 'Логин', type: 'text' },
+  { name: 'first_name', label: 'Имя', type: 'text' },
+  { name: 'second_name', label: 'Фамилия', type: 'text' },
+  { name: 'phone', label: 'Телефон', type: 'text' },
+  { name: 'password', label: 'Пароль', type: 'password' },
+  { name: 'passwordRepeat', label: 'Пароль (еще раз)', type: 'password' },
+];
+
+export const SignUpPage = () => (
+  <div className="signup-page">
+    <CardComponent inputs={inputs} />
+  </div>
+);
