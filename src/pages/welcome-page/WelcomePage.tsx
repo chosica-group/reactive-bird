beb195ef0cc72a45c6bb10488f3fa2cce4519322
Elena Kuznetsova <elenakuznetsova177@yandex.ru>
2022-06-08 08:48:28 +0300

    delete main page and router

--- src/pages/welcome-page/WelcomePage.tsx
+++
@@ -1,8 +0,0 @@
-import './WelcomePage.css';
-import { InfoAboutProject } from './components/infoAboutProject/index';
-
-export const WelcomePage = () => (
-  <div className="main-page">
-    <InfoAboutProject />
-  </div>
-);
--- src/pages/welcome-page/components/infoAboutProject/index.tsx
+++
@@ -1,1 +0,0 @@
-export { InfoAboutProject } from './InfoAboutProject';
