e3f516f7d4ed22e9c305333a60dd4553e162099a
Elena Kuznetsova <elenakuznetsova177@yandex.ru>
2022-06-08 08:54:54 +0300

    clean

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -2,6 +2,5 @@ import { compose } from '@reduxjs/toolkit';
 import type { FunctionComponent } from 'react';
 
 import { withTheme } from './with-theme';
-// import { withRouter } from './with-router/with-router';
 
 export const withProviders = compose<FunctionComponent>(withTheme);
