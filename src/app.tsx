dfe10e7937a748eb4a5bb1ae5ac3135aa8941f1d
Andrey.A <79037128326@ya.ru>
2022-06-10 09:42:44 +0300

    refactor: error boundary as provider

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,4 @@
-import { ErrorBoundary } from 'components/error-boundary';
 import { MainLayout } from 'layout/main';
 import { withProviders } from 'providers';
 
-export const App = withProviders(() => (
-  <ErrorBoundary>
-    <MainLayout />
-  </ErrorBoundary>
-));
+export const App = withProviders(() => <MainLayout />);
--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,5 +1,6 @@
 import type { FunctionComponent } from 'react';
 import { compose } from '@reduxjs/toolkit';
+import { withErrorBoundary } from './with-error-boundary';
 import { withTheme } from './with-theme';
 
-export const withProviders = compose<FunctionComponent>(withTheme);
+export const withProviders = compose<FunctionComponent>(withTheme, withErrorBoundary);
