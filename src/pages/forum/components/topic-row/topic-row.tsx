31dd2c6084a5c0ff8245913288fe781c942347f1
Alexey-Smolik <smolikvtanke@gmail.com>
2022-06-12 18:02:59 +0300

    chosica-30/forum-page
    Implemented forum page

---
+++ src/pages/forum/components/topic-row/topic-row.tsx
@@ -0,0 +1,30 @@
+import { Grid } from '@mui/material';
+import './topic-row.css';
+import { AddCircleRounded } from '@mui/icons-material';
+import Typography from '@mui/material/Typography';
+
+interface TProps {
+  title: string,
+  subTopicsCount: number,
+  answersCount: number
+}
+
+export const TopicRow = (data: TProps) => (
+  <><Grid item xs={8} zeroMinWidth>
+    <Typography noWrap component="div" className='grid-item title-item'>
+      <p className='title'>{data.title}</p>
+    </Typography>
+  </Grid>
+  <Grid item xs={1.6} zeroMinWidth>
+    <Typography noWrap component="div" className='grid-item count-item'>
+      <p>{data.subTopicsCount}</p>
+      <AddCircleRounded className='add-icon'></AddCircleRounded>
+    </Typography>
+  </Grid>
+  <Grid item xs={1.3} zeroMinWidth>
+    <Typography noWrap component="div" className='grid-item count-item'>
+      <p>{data.answersCount}</p>
+    </Typography>
+  </Grid></>
+);
+
