0b20b2bc8c4d6bcd3e095e3ea9d3db8d3b4ee62b
Elena Kuznetsova <elenakuznetsova177@yandex.ru>
2022-06-22 23:09:48 +0300

    in progress

--- src/pages/leaderboard/leaderboard.tsx
+++ src/pages/leaderboard/leaderboard.tsx
@@ -1,39 +1,48 @@
 // import React, { useEffect, useState } from 'react';
 import { Stack, Typography } from '@mui/material';
-// import { useGetAllLeaderboardQuery } from 'services/leaderboard/index';
+import { useGetAllLeaderboardQuery } from 'services/leaderboard/index';
+import type { TAllLeaderboardRequest } from 'services/leaderboard/types';
 import { Container, LeaderCard } from './components';
 
-const mockData = [
-  {
-    rating: 1,
-    time: 35,
-    result: 1,
-    name: 'Ваня',
-    avatar: '',
-  },
-  {
-    rating: 2,
-    time: 65,
-    result: 22,
-    name: 'Маша',
-    avatar: '',
-  },
-  {
-    rating: 3,
-    time: 35,
-    result: 15,
-    name: 'Петя',
-    avatar: '',
-  },
-];
+// const mockData = [
+//   {
+//     rating: 1,
+//     time: 35,
+//     result: 1,
+//     name: 'Ваня',
+//     avatar: '',
+//   },
+//   {
+//     rating: 2,
+//     time: 65,
+//     result: 22,
+//     name: 'Маша',
+//     avatar: '',
+//   },
+//   {
+//     rating: 3,
+//     time: 35,
+//     result: 15,
+//     name: 'Петя',
+//     avatar: '',
+//   },
+// ];
 
-export const LeaderboardPage = () => (
-  <Container>
-    <Typography variant="h1">Рекорды</Typography>
-    <Stack spacing={2} alignItems="center">
-      {mockData.map((user, index) => (
-        <LeaderCard key={index} {...user} />
-      ))}
-    </Stack>
-  </Container>
-);
+export const LeaderboardPage = () => {
+  const body: TAllLeaderboardRequest = {
+    ratingFieldName: 'rrrr',
+    cursor: 0,
+    limit: 0,
+  };
+  const { data } = useGetAllLeaderboardQuery(body);
+  return (
+    <Container>
+      <Typography variant="h1">Рекорды</Typography>
+      <Stack spacing={2} alignItems="center">
+        {data?.map((user, index) => (
+          <LeaderCard rating={0} time={0} result={0} name="dddd" avatar="wedwedwe" key={index} />
+        ))}
+      </Stack>
+    </Container>
+  );
+};
