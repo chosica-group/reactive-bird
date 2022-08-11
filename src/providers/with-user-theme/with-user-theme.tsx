4e72105dde1b6def80b808cb0eb9127deca91283
Elena Kuznetsova <elenakuznetsova177@yandex.ru>
2022-08-12 02:17:29 +0300

    back withUserTheme

--- src/providers/with-user-theme/with-user-theme.tsx
+++ src/providers/with-user-theme/with-user-theme.tsx
@@ -1,33 +1,34 @@
-// import { useEffect, useState } from 'react';
-// import { useDispatch } from 'react-redux';
-// import { useGetThemeQuery, useGetUserThemeQuery } from 'services/theme/theme-api';
-// import type { AuthState } from 'store/auth-reducer';
-// import { setUserTheme, setUserThemeName } from 'store/theme-reduser';
+import type { ComponentType } from 'react';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { userInfoSelector } from 'store/auth-reducer';
+import { useGetThemeQuery } from 'services/theme/theme-api';
+import { setUserThemeName, setUserTheme } from 'store/theme-reduser';
+import { useGetUserThemeQuery } from 'services/theme/theme-api';
 
-// export const WithUserTheme = (authState: AuthState) => {
-//   const [themeName, setThemeName] = useState('light');
-//   const [userId, setUserId] = useState(1);
-//   const [skip1, setSkip1] = useState(true);
-//   const [skip, setSkip] = useState(true);
-//   const dispatch = useDispatch();
-//   const { data } = useGetUserThemeQuery(userId, { skip });
-//   const { data: theme } = useGetThemeQuery(themeName, { skip: skip1 });
-//   // eslint-disable-next-line react/destructuring-assignment
-//   if (authState.isLoggedIn) {
-//     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-non-null-assertion, react/destructuring-assignment
-//     setUserId(authState.userId!);
-//     setSkip(false);
-//   }
-//   useEffect(() => {
-//     if (data && data.theme_name) {
-//       setSkip1(false);
-//       dispatch(setUserThemeName(data.theme_name));
-//     }
-//   }, [data, dispatch]);
-//   useEffect(() => {
-//     if (theme) {
-//       dispatch(setUserTheme(theme));
-//     }
-//   });
-//   // return <div />;
-// };
+export const withUserTheme = (Component: ComponentType) => () => {
+  const authState = useSelector(userInfoSelector);
+  const [themeName, setThemeName] = useState('light');
+  const [userId, setUserId] = useState(1);
+  const [skip1, setSkip1] = useState(true);
+  const [skip, setSkip] = useState(true);
+  const dispatch = useDispatch();
+  const { data } = useGetUserThemeQuery(userId, { skip });
+  const { data: theme } = useGetThemeQuery(themeName, { skip: skip1 });
+  if (authState.isLoggedIn) {
+    setUserId(authState.userId!);
+    setSkip(false);
+  }
+  useEffect(() => {
+    if (data && data.theme_name) {
+      setSkip1(false);
+      dispatch(setUserThemeName(data.theme_name));
+    }
+  }, [data, dispatch]);
+  useEffect(() => {
+    if (theme) {
+      dispatch(setUserTheme(theme));
+    }
+  });
+  return <Component />;
+};
