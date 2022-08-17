2c565adc0f6b0bb5f6ed77afe217ad380e48501f
Alexey-Smolik <smolikvtanke@gmail.com>
2022-08-17 20:40:23 +0300

    Test 2

--- src/server/models/index.ts
+++ src/server/models/index.ts
@@ -7,7 +7,7 @@ import { SiteThemeService } from 'server/services/site-theme';
 import { lightTheme, darkTheme } from './theme-data';
 
 const sequelizeOptions: SequelizeOptions = {
-  host: 'localhost', // если локально запуск - ставить localhost
+  host: !process.env.NODE_ENV || process.env.NODE_ENV === 'production' ? 'postgres' : 'localhost', // если локально запуск - ставить localhost
   // host: 'localhost',
   port: 5432,
   username: 'postgres',
@@ -20,16 +20,11 @@ export const sequelize = new Sequelize(sequelizeOptions);
 
 sequelize.addModels([Topics, Comments, UserTheme, SiteTheme]);
 
+Topics.hasMany(Comments, { as: 'comments', foreignKey: { name: 'topicId' } });
+Comments.belongsTo(Topics, { as: 'topic' });
+
 export async function dbConnect() {
   try {
-    await Topics.sync();
-    await Comments.sync();
-    await UserTheme.sync();
-    await SiteTheme.sync();
-
-    Topics.hasMany(Comments, { as: 'comments', foreignKey: { name: 'topicId' } });
-    Comments.belongsTo(Topics, { as: 'topic' });
-
     await sequelize.authenticate(); // Проверка аутентификации в БД
     await sequelize.sync(); // Синхронизация базы данных
     console.log('Connection has been established successfully.');
