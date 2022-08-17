dadbd5f455ece055335bd8c29eb4fa0c503f9308
Elena Kuznetsova <elenakuznetsova177@yandex.ru>
2022-08-17 10:49:10 +0300

    cors name back

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,5 @@
-const STATIC_CACHE_NAME = 's-cfp-app-v1';
-const DYNAMIC_CACHE_NAME = 'd-cfp-app-v1';
+const STATIC_CACHE_NAME = 's-cfp-app-v2';
+const DYNAMIC_CACHE_NAME = 'd-cfp-app-v2';
 const CACHE_URLS = [
     '/index.html',
     '/game',
