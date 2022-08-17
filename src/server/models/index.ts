1e03807332c16893bcca2ed5c4c15e6fce5d34e1
Alexey-Smolik <smolikvtanke@gmail.com>
2022-08-17 19:57:13 +0300

    Made improvements in sequelize models

--- src/server/models/index.ts
+++ src/server/models/index.ts
@@ -7,12 +7,12 @@ import { SiteThemeService } from 'server/services/site-theme';
 import { lightTheme, darkTheme } from './theme-data';
 
 const sequelizeOptions: SequelizeOptions = {
-  host: !process.env.NODE_ENV || process.env.NODE_ENV === 'production' ? 'postgres' : 'localhost', // если локально запуск - ставить localhost
+  host: 'localhost', // если локально запуск - ставить localhost
   // host: 'localhost',
   port: 5432,
   username: 'postgres',
-  password: 'magus-malawi-gush',
-  database: 'chosica',
+  password: 'popinara',
+  database: 'reactive-bird',
   dialect: 'postgres',
 };
 const siteThemeService = new SiteThemeService();
@@ -20,11 +20,16 @@ export const sequelize = new Sequelize(sequelizeOptions);
 
 sequelize.addModels([Topics, Comments, UserTheme, SiteTheme]);
 
-Topics.hasMany(Comments, { as: 'comments', foreignKey: { name: 'topicId' } });
-Comments.belongsTo(Topics, { as: 'topic' });
-
 export async function dbConnect() {
   try {
+    await Topics.sync();
+    await Comments.sync();
+    await UserTheme.sync();
+    await SiteTheme.sync();
+
+    Topics.hasMany(Comments, { as: 'comments', foreignKey: { name: 'topicId' } });
+    Comments.belongsTo(Topics, { as: 'topic' });
+
     await sequelize.authenticate(); // Проверка аутентификации в БД
     await sequelize.sync(); // Синхронизация базы данных
     console.log('Connection has been established successfully.');
