f74f9a03f8d6fab571ca68e8f6d9ad1d6323ca77
Alexey-Smolik <smolikvtanke@gmail.com>
2022-08-17 17:48:54 +0300

    Added routes

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,5 +1,5 @@
 export default {
   API_URL: 'https://ya-praktikum.tech/api/v2',
   API_APP_URL: '/my-app/v1',
-  SERVER_URL: 'http://localhost:3000',
+  SERVER_URL: 'https://chosica-reactive-bird-14.ya-praktikum.tech',
 };
